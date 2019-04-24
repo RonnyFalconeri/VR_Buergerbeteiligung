088316a241ceb58169986a4138c8129bab3ba2f3
RonnyFalconeri <ronny@falconeri.de>
2019-04-24 11:28:20 +0200

    Votingsystem scripts init

---
+++ Assets/Scripts/Votingsystem/Voter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voter : MonoBehaviour
+{
+    private Voting Voting;
+
+    public void Vote_Yes()
+    {
+        Voting.Vote_Yes();
+    }
+
+    public void Vote_No()
+    {
+        Voting.Vote_No();
+    }
+
+    public void Vote_Nothing()
+    {
+        Voting.Vote_Nothing();
+    }
+
+    public Voting Get_Voting()
+    {
+        // Get Voting object from Moderator, which instantiates the voting
+
+    }
+}
---
+++ Assets/Scripts/Votingsystem/Voting.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voting : MonoBehaviour
+{
+
+    private string Voting_Name;
+    private int Yes_Voters = 0;
+    private int No_Voters = 0;
+    private int Abstention_Voters = 0;
+    private int Amount_Voters = 0;
+
+
+    // Constructor
+    public Voting(string pVoting_Name)
+    {
+        this.Voting_Name = pVoting_Name;
+    }
+    
+
+    // Set voting values
+    public void Vote_Yes()
+    {
+        this.Yes_Voters++;
+        Debug.Log("Voted yes. " + this.Yes_Voters + " for 'Yes'");
+    }
+
+    public void Vote_No()
+    {
+        this.No_Voters++;
+        Debug.Log("Voted no. " + this.No_Voters + " for 'No'");
+    }
+
+    public void Vote_Nothing()
+    {
+        this.Abstention_Voters++;
+        Debug.Log("Voted nothing. " + this.Abstention_Voters + " for 'Nothing'");
+    }
+
+
+    // Get voting values
+    public int Get_Yes_Votes()
+    {
+        return this.Yes_Voters;
+    }
+
+    public int Get_No_Votes()
+    {
+        return this.No_Voters;
+    }
+
+    public int Get_Abstention_Votes()
+    {
+        return this.Abstention_Voters;
+    }
+}
