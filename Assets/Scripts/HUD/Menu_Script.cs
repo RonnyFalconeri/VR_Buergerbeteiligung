a4d7c7e2ae1883bf80be758eb9583b7a0f07b01f
max <max>
2019-05-06 13:39:17 +0200

    Log Out and HUD is in all rooms available

---
+++ Assets/Scripts/HUD/Menu_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+namespace VRRoom
+{
+
+    public class Menu_Script : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnClickLeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+            if (false == PhotonNetwork.InLobby)
+            {
+                Debug.Log("Joined default lobby.");
+                PhotonNetwork.JoinLobby();
+            }
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LobbyMenu");
+        } 
+    }
+}
