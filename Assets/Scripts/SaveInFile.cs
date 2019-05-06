9c32a107e9ded598d25bdab17da35b2aeefe71a1
RonnyFalconeri <ronny@falconeri.de>
2019-05-06 13:00:07 +0200

    write in file script

---
+++ Assets/Scripts/SaveInFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveInFile : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Write_File("C:\\Users\\ronny_f6nb3z1\\Desktop\\Blackboard_Media\\writing.txt", "Test war wieeederrr erfolgreich!");
+    }
+
+    public void Write_File(string path, string data)
+    {
+        //write in file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(data);
+        writer.Close();
+    }
+}
