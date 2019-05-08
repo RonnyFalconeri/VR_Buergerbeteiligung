7a8b7fb4c6746e54626b13d52a495d5fa3f3a58a
RonnyFalconeri <ronny@falconeri.de>
2019-05-08 11:03:22 +0200

    LoadImage ready

--- Assets/Scripts/Blackboards/LoadImage.cs
+++ Assets/Scripts/Blackboards/LoadImage.cs
@@ -1,42 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadImage : MonoBehaviour
 {
+    // Edit this two for each blackboard
+    public string Room, Name;
 
-    private string FilePath = "C:\\Users\\ronny_f6nb3z1\\Desktop\\Blackboard_Media\\west3.jpg";
+    private string FilePath;
     byte[] FileData;
     private RawImage Media_RawImage;
     private Texture2D texture = null;
-
 
 
     // Start is called before the first frame update
     void Start()
     {
 
-        if (File.Exists(FilePath)) // ERROR: The name 'File' does not exist in the current context?
+        FilePath = "C:\\Users\\ronny_f6nb3z1\\Desktop\\Blackboard_Media\\" + Room + "\\" + Name + ".jpg";
+
+        // if file exists, load the image
+        if (File.Exists(FilePath))
         {
-            FileData = File.ReadAllBytes(FilePath); // ERROR: The name 'File' does not exist in the current context?
+            FileData = File.ReadAllBytes(FilePath);
             texture = new Texture2D(2, 2);
             texture.LoadImage(FileData);
         }
 
+        // place the image on the canvas
         Media_RawImage = GetComponent<RawImage>();
         Media_RawImage.texture = texture;
 
     }
-
-    private void Update()
-    {
-        // Test Keyboard input for voting
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            print("space key was pressed");
-        }
-    }
-
 }
