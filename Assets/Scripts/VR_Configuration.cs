db8ec96a5b54a219191ce76fa70e7c04e66f66a4
JoshuaBurger <burgerj@dhbw-loerrach.de>
2019-05-11 20:31:44 +0200

    Detect whether VR glasses connected and deactivate mouse control in that case (else it disturbs VR controlers very bad). Also deactivate VR hands to avoid thousands of neverending error messages

---
+++ Assets/Scripts/VR_Configuration.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR;
+
+public class VR_Configuration : MonoBehaviour
+{
+    public GameObject hud;
+    public GameObject gameMenu;
+    public GameObject modVoteMenu;
+    public GameObject voteMenu;
+
+    public GameObject eventSystem;
+    public GameObject vrHands;
+
+    private GameObject[] controlables;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlables = new GameObject[4];
+        controlables[0] = hud;
+        controlables[1] = gameMenu;
+        controlables[2] = modVoteMenu;
+        controlables[3] = eventSystem;
+
+        configureVRControlability();
+    }
+
+    public void configureVRControlability()
+    {
+        bool enableVRcontrol = XRDevice.isPresent;
+
+        vrHands.SetActive(enableVRcontrol);
+
+        // configure whether elements shall be clicked by vr controlers or mouse
+        foreach (GameObject controlable in controlables)
+        {
+            // I don't know why but this OVRRaycaster is never found...But it's no big problem.
+            // We just leave it enabled, because it doesn't disturb mouse control (just the other way around)
+            ControllerSelection.OVRRaycaster vrray = (ControllerSelection.OVRRaycaster)controlable.GetComponent("OVR Raycaster");
+            if ( null != vrray )
+            {
+                vrray.enabled = enableVRcontrol;
+            }
+            GraphicRaycaster mouseRay = (GraphicRaycaster)controlable.GetComponent("GraphicRaycaster");
+            if (null != mouseRay)
+            {
+                mouseRay.enabled = !enableVRcontrol;
+            }
+        }
+
+        StandaloneInputModule mouseInput = (StandaloneInputModule)eventSystem.GetComponent("StandaloneInputModule");
+        if ( null != mouseInput )
+        {
+            mouseInput.enabled = !enableVRcontrol;
+        }
+        ControllerSelection.OVRInputModule vrInput = (ControllerSelection.OVRInputModule)eventSystem.GetComponent("OVRInputModule");
+        if (null != vrInput)
+        {
+            vrInput.enabled = enableVRcontrol;
+        }
+    }
+}
