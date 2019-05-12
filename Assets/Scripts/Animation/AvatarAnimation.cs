28de2f2329938e51a9eba96e12c365e51de9c42e
RonnyFalconeri <34677325+RonnyFalconeri@users.noreply.github.com>
2019-05-12 02:17:09 +0200

    animation added while idle or walking
    but walks like a zombie

--- Assets/Scripts/Animation/AvatarAnimation.cs
+++ Assets/Scripts/Animation/AvatarAnimation.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AvatarAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+    int idle = 0;
+    int walk = 1;
+
+    // Use this for initialization
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w"))
+        {
+            animator.SetInteger("state", walk);
+        }
+        else if (Input.GetKey("a"))
+        {
+            animator.SetInteger("state", walk);
+        }
+        else if (Input.GetKey("s"))
+        {
+            animator.SetInteger("state", walk);
+        }
+        else if (Input.GetKey("d"))
+        {
+            animator.SetInteger("state", walk);
+        }
+        else
+        {
+            animator.SetInteger("state", idle);
+        }
     }
 }
